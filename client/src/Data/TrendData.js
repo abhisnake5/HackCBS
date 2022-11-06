0176e454d005e9166c3c82e006b7872e188915b6
abhisnake5 <tiwariabhishek591@gmail.com>
2022-11-06 11:41:09 +0530

    Initial Commit

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData= [
+  {
+    name: "Cricket",
+    shares: 97,
+  },
+  {
+    name: "Kohli",
+    shares: 80.5,
+  },
+  {
+    name: "Reactjs",
+    shares: 72,
+  },
+  {
+    name: "MongoDB",
+    shares: 71.9,
+  },
+  {
+    name: "Need for Speed",
+    shares: 20,
+  },
+];
